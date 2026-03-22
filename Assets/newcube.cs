78de718df9fba81facdf868ba06cf0c7bde8a98d
krill239 <kirill17sv@gmail.com>
2026-03-22 12:52:53 +0300

    anima weapun

--- Assets/newcube.cs
+++ Assets/newcube.cs
@@ -2,46 +2,79 @@ using UnityEngine;
 
 public class newcube : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private int age = 14;
-    public float speed = 0.1f;
-    public float speedJump = 0.1f;
+    public float speed = 5f;
+    public float jumpForce = 5f;
     public int heal = 100;
     public float maxDamage = 8f;
     public float minDamage = 3f;
     public float Damage;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
     void Start()
     {
         // Debug.Log(age);
+        rb = GetComponent<Rigidbody>();
+
+        // Если Rigidbody не найден, добавляем его
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Движение
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(new Vector3(0, 0, -1) * speed * Time.deltaTime);
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        // Прыжок с проверкой на земле
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            transform.Translate(new Vector3(0, 1, 0) * speedJump * Time.deltaTime);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        // Рандомный урон
         if (Input.GetKey(KeyCode.R))
         {
-            Damage = Random.Range(maxDamage, minDamage);
+            Damage = Random.Range(minDamage, maxDamage);
             Debug.Log(Damage);
         }
+    }
+
+    // Проверка касания земли
+    void OnCollisionStay(Collision collision)
+    {
+        // Проверяем, что объект касается земли по нормали
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (contact.normal.y > 0.5f) // Если нормаль направлена вверх (земля)
+            {
+                isGrounded = true;
+                return;
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
     }
 }
